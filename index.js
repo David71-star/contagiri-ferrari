ed72d03069f52b9db26cc0f5a5303e8a46e0833b
David71-star <146646442+David71-star@users.noreply.github.com>
2025-08-24 17:58:07 +0200

    all ok

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const contagiri = document.querySelector('.contagiri');
+const scocca = document.querySelector('.bar');
+const interno = document.querySelector('.contagiri-interno');
+
 
 const numbers = [];
+const bar =[];
+const line = [];
 
 for (let i = 0; i <= 12; i++) {
     numbers.push(`<span style="--index:${i};"><p>${i}</p></span>`);
@@ -8,3 +13,19 @@ for (let i = 0; i <= 12; i++) {
 console.log(numbers);
 
 contagiri.insertAdjacentHTML('afterbegin', numbers.join(''));
+
+for (let i = 1; i <= 72; i++) {
+    bar.push(`<span style="--index:${i};"><p></p></span>`);
+}
+console.log(bar);
+scocca.insertAdjacentHTML('afterbegin', bar.join(''));
+
+for (let i = 1; i <= 4; i++) {
+    line.push(`<div class="line" style="--index:${i};"></div>`);
+}
+console.log(line);
+interno.insertAdjacentHTML('afterbegin', line.join(''));
+
+
+
+
